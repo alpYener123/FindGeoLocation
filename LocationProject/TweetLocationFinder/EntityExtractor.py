b69a84e6f560a66ffa226a02818696be27a9dc27
alpYener123 <alpondery@gmail.com>
2023-08-29 21:13:23 +0300

    Added functions in EntityExtractor.py

--- LocationProject/TweetLocationFinder/EntityExtractor.py
+++ LocationProject/TweetLocationFinder/EntityExtractor.py
@@ -1,5 +1,6 @@
 # This class is for finding location entities in a given tweet
 
+from unidecode import unidecode
 from langdetect import detect
 import spacy
 from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
@@ -8,19 +9,25 @@ from transformers import pipeline
 
 class EntityExtractor:
 
+    def __init__(self):
+        self.nlp = spacy.load('en_core_web_lg')
+        self.tokenizer = AutoTokenizer.from_pretrained("busecarik/bert-loodos-sunlp-ner-turkish")
+        self.model = AutoModelForTokenClassification.from_pretrained("busecarik/bert-loodos-sunlp-ner-turkish")
+        self.clf = pipeline("ner", model=self.model, tokenizer=self.tokenizer)
+
+
     # detects the language
     @staticmethod
-    def detect_lang(tweet):
+    def detect_lang(self, tweet):
         return (detect(tweet))
 
     # returns a list of entities in the sentence
     @staticmethod
-    def give_loc_ents(tweet, lang):
+    def give_loc_ents(self, tweet, lang):
 
         return_list = []
         if lang == "en":
-            nlp = spacy.load('en_core_web_lg')
-            doc = nlp(tweet)
+            doc = self.nlp(tweet)
             for ent in doc.ents:
                 if ent.label_ == "FAC" or ent.label_ == "ORG" or ent.label_ == "GPE" or ent.label_ == "LOC":
                     return_list.append(ent.text)
@@ -28,14 +35,12 @@ class EntityExtractor:
             return return_list
 
         elif lang == "tr":
-            tokenizer = AutoTokenizer.from_pretrained("busecarik/bert-loodos-sunlp-ner-turkish")
-            model = AutoModelForTokenClassification.from_pretrained("busecarik/bert-loodos-sunlp-ner-turkish")
-            clf = pipeline("ner", model=model, tokenizer=tokenizer)
+            
             tweet = tweet.replace("'", " ")
             word_list = tweet.split(" ")
-            nlp = clf(tweet)
+            nlp = self.clf(tweet)
             for i in range(0, len(nlp)):
-                if nlp[i]["entity"] == "B-ORGANIZATION":
+                '''if nlp[i]["entity"] == "B-ORGANIZATION":
                     add = ""           
                     word = nlp[i]["word"].replace("#", "")
                     for item in word_list:
@@ -48,15 +53,15 @@ class EntityExtractor:
                             word = nlp[d]["word"].replace("#", "")
                             for item in word_list:
                                 if word in item:
-                                    if item not in return_list:
+                                    if item not in return_list and item not in add:
                                         add += (item + " ")
                             if d < (len(nlp)-1):
                                 d = d+1
                             else:
                                 break
-                    return_list.append(add[:-1])
+                    return_list.append(add[:-1])'''
 
-                elif nlp[i]["entity"] == "B-LOCATION":
+                if nlp[i]["entity"] == "B-LOCATION":
                     add = ""           
                     word = nlp[i]["word"].replace("#", "")
                     for item in word_list:
@@ -69,12 +74,25 @@ class EntityExtractor:
                             word = nlp[d]["word"].replace("#", "")
                             for item in word_list:
                                 if word in item:
-                                    if item not in return_list:
+                                    if item not in return_list and item not in add:
                                         add += (item + " ")
                             if d < (len(nlp)-1):
                                 d = d+1
                             else:
                                 break
+                    add = add[:-1]
+                    add = unidecode(add.strip())
+                    add = add.lower()
                     return_list.append(add[:-1])
 
             return return_list
+
+
+def is_substring_in_nested_dict(dct, target_string):
+    for key, value in dct.items():
+        if isinstance(value, dict):
+            if is_substring_in_nested_dict(value, target_string):
+                return True
+        elif isinstance(key, str) and key in target_string:
+            return key
+    return "Could not find"
