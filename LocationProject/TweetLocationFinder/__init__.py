d4a2edaff8221963fcea24e712705eadcda688cf
alpYener123 <alpondery@gmail.com>
2023-09-10 18:21:50 +0300

    Started new file and added function

--- LocationProject/TweetLocationFinder/__init__.py
+++ LocationProject/TweetLocationFinder/__init__.py
@@ -1,3 +1,3 @@
 from .EntityExtractor import EntityExtractor
-from .GatherUserLoc.GatherFiles import GatherFiles
+from .GatherUserLoc.GatherFiles import GatherFiles, back_to_forward_slash
 from .GatherUserLoc.GatherLoc import GatherLoc
